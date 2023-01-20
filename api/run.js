7308d7dd3c1c1c6ed3225ee764433779a0094a97
Natrexq <69844485+Natrexq@users.noreply.github.com>
2023-01-20 18:56:06 +0100

    Update run.js

--- api/run.js
+++ api/run.js
@@ -1,6 +1,23 @@
-/*How to set up script and run script */
-/*1. Define where the notification should be displayed  [body,div]*/
-/*2. Call function for example after click button on anything other */
+/*! How to run script */
+/*3 Full docs on GitHub: https://github.com/Natrexq/xMess-API-#readme*/
+
+/*How to call notification:*/
+
+  /*  1. Define where the notification will be displayed by makeing varable with hooked container [body,div] */
+  /*  2. Define when notification will be display*/
+  /*  3: Enter data into function:*/
+
+      
+   /* xMassCall(F1, "F2", "F3", "F4", "F5");*/
+
+
+  /*  What is the meaning of these "F"?*/
+    /* F1: where notification will be displayed (div,body);*/
+     /* F2: Animation: [from-left,from-right,from-top,from-bottom];*/
+     /* F3: Type of notification: [warn, help, ads, info];*/
+         /* F4: Notification title: "title of notification";*/
+         /*  F5: Notification message: "message of notification";*/
+        
 
 /*!!!! IMPORTANT - Currently for the api to work run script must be next to api js file and api folder :/
     In future version of api i will fix it
